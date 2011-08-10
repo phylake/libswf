3fcadf2fc6cf1e38b4225d9e90c1e9a0fc223579
Brandon Cook <phylake@gmail.com>
2011-08-09 21:43:05 -0700

    consolidated data types into swf.h

--- swf.cpp
+++ swf.cpp
@@ -1,9 +1,230 @@
 #include "swf.h"
 
 //-----------------------------------------
+//                 AbstractBase
+//-----------------------------------------
+swf::AbstractBase::~AbstractBase() {}
+
+//-----------------------------------------
+//                 AbstractData
+//-----------------------------------------
+unsigned int swf::AbstractData::getUBits(unsigned char * ptr, unsigned int n, unsigned int startAt = 0) {
+	unsigned int ret = 0;
+	unsigned int i = 0;
+	unsigned int pos;
+	unsigned int t;
+	unsigned int scale;
+	
+	//pointer adjustment
+	ptr += (unsigned int) floor(startAt / 8);
+	
+	//scaling
+	startAt = startAt % 8;
+	n = n % 32;
+	
+	do {
+		scale = (i + startAt) % 8;
+		
+		/* shift sizeof(char) * 8 - 1 scaled by i plus a starting position */
+		pos = 8 - 1 - scale;
+		if( i > 0 && scale == 0 )
+		{
+			ptr++;
+		}
+		
+		t = *ptr >> pos;
+		
+#ifdef DEBUG
+		printf("%i", t & 1);
+#endif
+		
+		ret <<= 1;
+		ret |= t & 1;
+	} while (++i < n);
+	
+#ifdef DEBUG
+	printf("\n");
+#endif
+	
+	return ret;
+}
+
+signed int swf::AbstractData::getSBits(unsigned char * ptr, unsigned int n, unsigned int startAt = 0) {
+	unsigned int ret = AbstractData::getUBits(ptr, n, startAt);
+	
+	if ( ret & (1<<(n-1)) )
+	{//msb is 1
+		ret |= ((~0) << n);
+	}
+	
+	return ret;
+}
+
+swf::AbstractData::~AbstractData() {}
+
+//-----------------------------------------
+//                   U8
+//-----------------------------------------
+void swf::U8::fromSWF( char *& ptr ) {
+	value = ptr[0] & 0x000000ff;
+	ptr++;
+}
+
+unsigned char swf::U8::toBE() {
+	return value;
+}
+
+//-----------------------------------------
+//                   U16
+//-----------------------------------------
+void swf::U16::fromSWF( char *& ptr ) {
+	value = 
+	ptr[0] <<  0 & 0x000000ff |
+	ptr[1] <<  8 & 0x0000ff00
+	;
+	ptr += 2;
+}
+
+unsigned short int swf::U16::getValue() {
+	return value;
+}
+
+double swf::U16::toFixed8() {
+	double decimal = 0;
+	
+	if (value && 0x00ff)
+	{
+		decimal = (double)(value & 0x00ff) / pow(10, ceil(log10(pow(2, 8))));
+	}
+	
+	return ((value & 0xff00) >> 8) + decimal;
+}
+
+float swf::U16::toFloat() {
+	unsigned ret = 0;
+	//int s = (value << 16) & 0x80000000;
+	//int e = (value >> 10) & 0x1f;
+	//int m = value & 0x3ff;
+	
+	
+	
+	return ret;
+}
+
+//-----------------------------------------
+//                   U32
+//-----------------------------------------
+swf::U32::U32(unsigned value) {
+	if (value) {
+		(*this).value = value;
+	}
+}
+
+void swf::U32::fromSWF( char *& ptr ) {
+	value = 
+	ptr[0] <<  0 & 0x000000ff |
+	ptr[1] <<  8 & 0x0000ff00 |
+	ptr[2] << 16 & 0x00ff0000 |
+	ptr[3] << 24 & 0xff000000
+	;
+	ptr += 4;
+}
+
+unsigned int swf::U32::getValue() {
+	return value;
+}
+
+double swf::U32::toFixed16() {
+	double decimal = 0;
+	
+	if (value && 0x0000ffff)
+	{
+		decimal = (double)(value & 0x0000ffff) / pow(10, ceil(log10(pow(2, 16))));
+	}
+	
+	return ((value & 0xffff0000) >> 16) + decimal;
+}
+
+float swf::U32::toFloat() {
+	return *(float *)&value;
+}
+
+//-----------------------------------------
+//                   Twip
+//-----------------------------------------
+signed int swf::Twip::toPX() {
+	return value/20;
+}
+
+//-----------------------------------------
+//                   RECT
+//-----------------------------------------
+void swf::RECT::fromSWF( char *& ptr ) {
+	int i = 0;
+	
+	nBits      = AbstractData::getUBits((unsigned char *)ptr, 5);
+	xMin.value = AbstractData::getSBits((unsigned char *)ptr, nBits, 5 + nBits * i++);
+	xMax.value = AbstractData::getSBits((unsigned char *)ptr, nBits, 5 + nBits * i++);
+	yMin.value = AbstractData::getSBits((unsigned char *)ptr, nBits, 5 + nBits * i++);
+	yMax.value = AbstractData::getSBits((unsigned char *)ptr, nBits, 5 + nBits * i++);
+	
+	double shift = ceil( (5 + (double)(nBits * i)) / 8);
+	ptr += (unsigned int)shift;
+	
+#ifdef DEBUG
+	printf("i: %i\n", i);
+	printf("nBits: %i\n", nBits);
+	printf("xMin: %i\n", xMin.toPX());
+	printf("xMax: %i\n", xMax.toPX());
+	printf("yMin: %i\n", yMin.toPX());
+	printf("yMax: %i\n", yMax.toPX());
+	printf("shift %f\n", shift);
+#endif
+}
+
+unsigned int swf::RECT::toBE() {
+	return 0;
+}
+
+//-----------------------------------------
+//                RecordHeader
+//-----------------------------------------
+void swf::RecordHeader::fromSWF( char *& stream ) {
+	tagCodeAndLength.fromSWF(stream);
+	
+	tag = (tagCodeAndLength.getValue() >> 6) & 0x3ff;
+	
+	/*
+	 check the last 6 bits of U16
+	 All 1's means this is a long header
+	 */
+	if ( ~(tagCodeAndLength.getValue() && 0x3f) == 0 ) {
+		isShort = false;
+		tagLength.fromSWF(stream);
+	} else {
+		isShort = true;
+	}
+}
+/*class RecordHeader {
+ U16 tcal;//TagCodeAndLength
+ U32 length;
+ bool isShort;
+ public:
+ void fromSWF( char * stream );
+ short int type();
+ unsigned int length();
+ };*/
+
+
+//-----------------------------------------
+//                AbstractTag
+//-----------------------------------------
+swf::AbstractTag::~AbstractTag() {}
+
+//-----------------------------------------
 //                SWFHeader
 //-----------------------------------------
-void swf::SWFHeader::fromSWF( char * stream) {
+void swf::SWFHeader::fromSWF( char *& stream) {
 	_compressed = stream[0] == 'C';
 	
 	stream += 3;//skip 'W' and 'F'
@@ -22,6 +243,8 @@ void swf::SWFHeader::continueWith(char *& stream) {
 	printf("frameCount %i\n", _frameCount.getValue());
 	#endif
 }
+
+swf::SWFHeader::~SWFHeader() {}
 
 swf::U32 swf::SWFHeader::fileLength() {
 	return _fileLength;
--- swf.h
+++ swf.h
@@ -2,52 +2,175 @@
 #define SWF_H
 
 #include <stdio.h>
-#include "types.h"
+#include "math.h"
 
-namespace swf {
+namespace swf
+{
 	
-class TagBase {
-protected:
-	TagBase *next;
-	TagBase *prev;
-	char * _buffer;
-public:
-
-};
-
-class SWFHeader : public TagBase {
-	bool _compressed;
-	U8 _version;
-	U32 _fileLength;
-	RECT _frameSize;
-	U16 _frameRate;
-	U16 _frameCount;
-public:
-	void fromSWF(char * stream);
-	void continueWith(char *& stream);
-	
+	//--------------------------------------------------------------
 	//
-	//accessors
+	//			               ABSTRACT BASE
 	//
-	bool compressed();
-	U32	fileLength();
-	RECT frameSize();
-};
-
-class SWF {
-	char ** _buffer;
-public:
-	SWFHeader header;
+	//--------------------------------------------------------------
 	
-	void fromSWF(char *& stream);
-	void continueWith(char *& stream);
+	//-----------------------------------------
+	//                AbstractBase
+	//-----------------------------------------
+	class AbstractBase {
+	public:
+		virtual void fromSWF( char *& ptr) = 0;
+		virtual ~AbstractBase() = 0;
+	};
 	
-	void toSWF();
-	
+	//--------------------------------------------------------------
 	//
-	//accessors
+	//			               DATA TYPES
 	//
-};
+	//--------------------------------------------------------------
+	
+	//-----------------------------------------
+	//                AbstractData
+	//-----------------------------------------
+	class AbstractData : public AbstractBase {
+	public:
+		static unsigned int getUBits(unsigned char * ptr, unsigned int n, unsigned int startAt);
+		static   signed int getSBits(unsigned char * ptr, unsigned int n, unsigned int startAt);
+		
+		virtual ~AbstractData() = 0;
+	};
+	
+	//-----------------------------------------
+	//                   U8
+	//-----------------------------------------
+	class U8 : public AbstractData {
+		unsigned char value;
+	public:
+		virtual void fromSWF( char *& ptr );
+		unsigned char toBE();
+	};
+	
+	//-----------------------------------------
+	//                   U16
+	//-----------------------------------------	
+	class U16 : public AbstractData {
+		unsigned short int value;
+	public:
+		virtual void fromSWF( char *& ptr );
+		double toFixed8();
+		float toFloat();
+		unsigned short int getValue();
+	};
+	
+	//-----------------------------------------
+	//                   U32
+	//-----------------------------------------	
+	class U32 : public AbstractData {
+		unsigned int value;
+	public:
+		U32(unsigned value = 0);
+		virtual void fromSWF( char *& ptr );
+		double toFixed16();
+		float toFloat();
+		unsigned int getValue();
+	};
+	
+	//-----------------------------------------
+	//                   Twip
+	//-----------------------------------------	
+	class Twip {
+		
+	public:
+		signed int value;
+		signed int toPX();
+	};
+	
+	//-----------------------------------------
+	//                   RECT
+	//-----------------------------------------
+	class RECT : public AbstractData {
+	public:
+		unsigned char nBits;
+		Twip xMin;
+		Twip xMax;
+		Twip yMin;
+		Twip yMax;
+		virtual void fromSWF( char *& ptr );
+		unsigned int toBE();
+	};
+	
+	//--------------------------------------------------------------
+	//
+	//			                   TAGS
+	//
+	//--------------------------------------------------------------
+	
+	//-----------------------------------------
+	//               RecordHeader
+	//-----------------------------------------
+	class RecordHeader {
+		U16 tagCodeAndLength;
+		U32 tagLength;
+		short int tag;
+		bool isShort;
+	public:
+		virtual void fromSWF( char *& stream );
+		short int type();
+		unsigned int length();
+	};
+	
+	//-----------------------------------------
+	//                AbstractTag
+	//-----------------------------------------
+	class AbstractTag : public AbstractBase {
+	protected:
+		AbstractTag *next;
+		AbstractTag *prev;
+		char * _buffer;
+	public:
+		RecordHeader recordHeader;
+		virtual ~AbstractTag() = 0;
+	};
+	
+	//-----------------------------------------
+	//                 SWFHeader
+	//-----------------------------------------
+	class SWFHeader : public AbstractTag {
+		bool _compressed;
+		U8 _version;
+		U32 _fileLength;
+		RECT _frameSize;
+		U16 _frameRate;
+		U16 _frameCount;
+	public:
+		virtual void fromSWF(char *& stream);
+		void continueWith(char *& stream);
+		virtual ~SWFHeader();
+		
+		//
+		//accessors
+		//
+		bool compressed();
+		U32	fileLength();
+		RECT frameSize();
+	};
+	
+	//-----------------------------------------
+	//                    SWF
+	//-----------------------------------------
+	class SWF {
+		char ** _buffer;
+	public:
+		SWFHeader header;
+		
+		virtual void fromSWF(char *& stream);
+		void continueWith(char *& stream);
+		
+		void toSWF();
+		
+		//
+		//accessors
+		//
+	};
 
 }
 #endif
